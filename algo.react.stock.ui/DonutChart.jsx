a01913341e6c68eda16bbd6e4a7f57872b442b7a
algo-rgolla <ravi.golla@hotmail.com>
2026-06-08 22:00:09 +1000

    Updated tyhe Donut chart

--- algo.react.stock.ui/DonutChart.jsx
+++ algo.react.stock.ui/DonutChart.jsx
@@ -1,2 +1,3 @@
 import { getChartColorsArray } from './ChartsDynamicColor';
 import { getChartColorsArray } from './ChartsDynamicColor';
+// This is Donut chart component, it is used to display the data in a circular format. It is a variation of the pie chart, where the center of the circle is cut out, creating a donut shape. The size of each segment of the donut represents the proportion of the data it represents.
